6d29d09f1746eaf677417168fac088b84361a363
Alex Metelli <alexmetelli.poker@gmail.com>
2020-12-28 16:11:46 +0000

    helpers tested

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Stores players details
 // Name: self descriptive, symbol: 'X' or 'O' at choise.
 type player struct {
@@ -9,9 +11,12 @@ type player struct {
 
 func main() {
 
-	var p1 = player{"Alex", "X"}
-	board := makeBoard()
-	var coord = [2]int{1, 2}
-	board = insertToBoard(p1.symbol, board, coord)
-	printBoard(board)
+	//var p1 = player{"Alex", "X"}
+	var board = [3][3]string{{"X", "-", "-"},
+		{"X", "-", "-"},
+		{"-", "-", "-"},
+	}
+	//var coord = [2]int{1, 2}
+	//board = insertToBoard(p1.symbol, board, coord)
+	fmt.Println(checkWin(board))
 }
